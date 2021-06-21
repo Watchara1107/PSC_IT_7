1d76cb41b930de160742b13e2526f9262a88def3
Watchara Ketkaew <wk996110@gmailcom>
2021-06-21 12:05:45 +0700

    update week6

--- week6/content.php
+++ week6/content.php
@@ -2,9 +2,12 @@
     //require("menu");
     include("menu.php");
     $job = "วิชาการพัฒนาโปรแกรมบนอุปกรณ์เคลื่อนที่ <br>";
+    $job = "วิชาไมโครโปรเซสเซอร์เบื้องต้น<br>";
     echo $job."<br>";
     include("footer.php");
     //require("footer.php");
     //include("menu.php");
+
+
 
 ?>
