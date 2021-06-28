69d45f779c729ec9651fd3a44022c585579375a9
Watchara Ketkaew <wk996110@gmailcom>
2021-06-28 10:35:41 +0700

    week6

---
+++ week7/array2.php
@@ -0,0 +1,12 @@
+<?php 
+    //Array แบบเดี่ยว
+    $number = [10,20,30,10,20,30,10,20,30,40];
+    //echo $number[1];
+
+    //Array แบบคู่
+    $color = ["yellow"=>"สีเหลือง","red"=>"สีเหลือง","orange"=>"สีส้ม"];
+    print_r($color);
+    echo "<br>";
+    echo $color["red"];
+
+?>
